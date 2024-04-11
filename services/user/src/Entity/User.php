3325da9771ed74228bd520408cdffa96dd9ebbb2
Ivan Balashov <bigracooon@gmail.com>
2024-04-11 13:32:07 +0400

    Add base structure

---
+++ services/user/src/Entity/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    private ?Uuid $id = null;
+
+    #[ORM\Column(length: 255)]
+    private string $fullname;
+
+    public function getId(): ?Uuid
+    {
+        return $this->id;
+    }
+}
