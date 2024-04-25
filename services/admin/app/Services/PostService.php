92b14864dfc4ce77c16cde6377bf0640080c95fd
balashov.i <balashov.i@optimax.dev>
2024-04-25 12:05:37 +0400

    Add exception handling to PostService
    The commit introduces an exception handling mechanism in the PostService. Now, when the

--- services/admin/app/Services/PostService.php
+++ services/admin/app/Services/PostService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use Exception;
 
 class PostService
 {
@@ -12,8 +13,14 @@ class PostService
      */
     public function getPosts(): array
     {
-        $client = new Client();
-        $response = $client->request('GET', 'http://172.28.0.1:8069/api/post');
-        return json_decode($response->getBody()->getContents(), true);
+        $client = new Client(['timeout' => 10]);
+        try {
+            $response = $client->request('GET', 'http://172.29.0.1:8069/api/post');
+            $response = json_decode($response->getBody()->getContents(), true);
+        } catch (Exception $exception) {
+            $response = [];
+        }
+
+        return $response;
     }
 }
