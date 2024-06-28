99f3063646fa820f5f9b6067ab6dbad06536cf74
i.balashov <bigracooon@gmail.com>
2024-06-28 22:02:27 +0400

    Test

--- services/post/app/Http/Controllers/PostController.php
+++ services/post/app/Http/Controllers/PostController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Collection;
 
 class PostController extends Controller
 {
+    public function index(): Collection
+    {
+        return Post::all();
+    }
 }
